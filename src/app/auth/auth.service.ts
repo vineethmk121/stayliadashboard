279096f86f09190c96cd3c249daef7675e6dee6a
Sooraj Dev Varrier <soorajdev93@gmail.com>
2022-03-12 15:46:31 +0530

    add login

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+  login(data:any):Observable<any>{
+    return this.http.post(`${environment.apiBaseURL}/admin/users/login`,data);
+  }
+}
--- src/app/layout/authenticated/authenticated-routing.module.ts
+++ src/app/layout/authenticated/authenticated-routing.module.ts
@@ -1,73 +1,90 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/auth.guard';
 import { AgentListComponent } from 'src/app/user/agent-list/agent-list.component';
 import { AuthenticatedComponent } from './authenticated.component';
 
 const routes: Routes = [
   {
     path:'dashboard',
-    loadChildren:()=>import('../../dashboard/dashboard.module').then(m=>m.DashboardModule)
+    loadChildren:()=>import('../../dashboard/dashboard.module').then(m=>m.DashboardModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'user',
-    loadChildren:()=>import('../../user/user.module').then(m=>m.UserModule)
+    loadChildren:()=>import('../../user/user.module').then(m=>m.UserModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'agent',
-    component:AgentListComponent
+    component:AgentListComponent,
+    canActivate:[AuthGuard]
   },
   {
     path:'property',
-    loadChildren:()=>import('../../property/property.module').then(m=>m.PropertyModule)
+    loadChildren:()=>import('../../property/property.module').then(m=>m.PropertyModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'property-type',
-    loadChildren:()=>import('../../property-type/property-type.module').then(m=>m.PropertyTypeModule)
+    loadChildren:()=>import('../../property-type/property-type.module').then(m=>m.PropertyTypeModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'property-category',
-    loadChildren:()=>import('../../property-categories/property-category.module').then(m=>m.PropertyCategoryModule)
+    loadChildren:()=>import('../../property-categories/property-category.module').then(m=>m.PropertyCategoryModule),
+    canActivate:[AuthGuard]
   },
  
   {
     path:'additional-information',
-    loadChildren:()=>import('../../additional-information/additional-information.module').then(m=>m.AdditionalInformationModule)
+    loadChildren:()=>import('../../additional-information/additional-information.module').then(m=>m.AdditionalInformationModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'amenities',
-    loadChildren:()=>import('../../amenities/amenities.module').then(m=>m.AmenitiesModule)
+    loadChildren:()=>import('../../amenities/amenities.module').then(m=>m.AmenitiesModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'bedroom-type',
-    loadChildren:()=>import('../../bedroom-type/bedroom-type.module').then(m=>m.BedroomTypeModule)
+    loadChildren:()=>import('../../bedroom-type/bedroom-type.module').then(m=>m.BedroomTypeModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'furnishing-type',
-    loadChildren:()=>import('../../furnishing-type/furnishing-type.module').then(m=>m.FurnishingTypeModule)
+    loadChildren:()=>import('../../furnishing-type/furnishing-type.module').then(m=>m.FurnishingTypeModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'nationality',
-    loadChildren:()=>import('../../nationality/nationality.module').then(m=>m.NationalityModule)
+    loadChildren:()=>import('../../nationality/nationality.module').then(m=>m.NationalityModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'overview',
-    loadChildren:()=>import('../../overview/overview.module').then(m=>m.OverviewModule)
+    loadChildren:()=>import('../../overview/overview.module').then(m=>m.OverviewModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'specality',
-    loadChildren:()=>import('../../specality/specality.module').then(m=>m.SpecalityModule)
+    loadChildren:()=>import('../../specality/specality.module').then(m=>m.SpecalityModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'tag',
-    loadChildren:()=>import('../../tag/tag.module').then(m=>m.TagModule)
+    loadChildren:()=>import('../../tag/tag.module').then(m=>m.TagModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'filter',
-    loadChildren:()=>import('../../filter/filter.module').then(m=>m.FilterModule)
+    loadChildren:()=>import('../../filter/filter.module').then(m=>m.FilterModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'faq',
-    loadChildren:()=>import('../../faq/faq.module').then(m=>m.FAQModule)
+    loadChildren:()=>import('../../faq/faq.module').then(m=>m.FAQModule),
+    canActivate:[AuthGuard]
   },
   {path:'',redirectTo:'/dashboard',pathMatch:'full'}
 ];
